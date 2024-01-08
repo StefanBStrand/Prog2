cc65668129d396c07e512adb1a918c3795c785ee
Stefan Strand <stefanboytchev@hotmail.com>
2024-01-08 20:49:26 +0100

    refactored files

---
+++ Oblig1/MoonGravity.java
@@ -0,0 +1,8 @@
+public class MoonGravity {
+    public static void main(String[] args) {
+        double weight = 95;
+        double moonWeight = weight * 0.165;
+        System.out.println("My weight on the moon is " + moonWeight + " kilograms");
+    }
+}
+
