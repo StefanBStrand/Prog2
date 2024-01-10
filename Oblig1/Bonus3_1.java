79a9ecbde00e8c05fa660c8180e40062a5026338
Stefan Strand <stefanboytchev@hotmail.com>
2024-01-10 19:59:00 +0100

    added bonus tasks. 3.1 and 3.2 complete

--- Oblig1/Bonus3_1.java
+++ Oblig1/Bonus3_1.java
@@ -3,5 +3,33 @@ public class Bonus3_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        boolean registering = true; // Code added for oppgave 3.2
+
+        while (registering) {  // Code added for oppgave 3.2
+
+            System.out.print("Enter the name of the planet:");
+            String planetName = scanner.nextLine();
+
+            System.out.print("Enter the radius of the planet:");
+            double planetRadius = scanner.nextDouble();
+
+            System.out.print("Enter the mass of the planet:");
+            double planetMass = scanner.nextDouble();
+
+            scanner.nextLine();
+
+            Planet planet = new Planet(planetName, planetRadius, planetMass);
+
+            System.out.println(planet.getNavn() + " has a radius of " + planet.getRadius() + "km and a mass of " +
+                    planet.getMasse() + "kg");
+
+            // Code added for oppgave 3.2
+            System.out.print("Do you wish to continue registering planets? Answer yes or no: ");
+            String answer = scanner.nextLine();
+
+            if (answer.equals("no")) {
+                registering = false;
+            }
+        }
     }
 }
