7bdd7791d03ed33c4e84db1cce8d9ee36acb02a4
Stefan Strand <stefanboytchev@hotmail.com>
2024-01-14 14:26:38 +0100

    updated bonus task

--- Oblig1/Bonus3_3.java
+++ Oblig1/Bonus3_3.java
@@ -17,41 +17,50 @@ public class Bonus3_3 {
                     " print to PRINT planets. Type update to UPDATE an entry. Choose what you want to do: ");
             String menuChoices = scanner.nextLine();
 
-            if (menuChoices.equals("add")) {
+            switch (menuChoices) {
+                case "add" -> {
 
-                System.out.print("Enter the name of the planet:");
-                String planetName = scanner.nextLine();
+                    System.out.print("Enter the name of the planet:");
+                    String planetName = scanner.nextLine();
 
-                System.out.print("Enter the radius of the planet:");
-                double planetRadius = scanner.nextDouble();
+                    System.out.print("Enter the radius of the planet:");
+                    double planetRadius = scanner.nextDouble();
 
-                System.out.print("Enter the mass of the planet:");
-                double planetMass = scanner.nextDouble();
+                    System.out.print("Enter the mass of the planet:");
+                    double planetMass = scanner.nextDouble();
 
-                scanner.nextLine(); //Used to "eat up" the empty line after scanner reads double.
+                    scanner.nextLine(); //Used to "eat up" the empty line after scanner reads double.
 
-                Planet planet = new Planet(planetName, planetRadius, planetMass);
 
-                planets.add(planet);
+                    Planet planet = new Planet(planetName, planetRadius, planetMass);
 
-                System.out.println(planet.getNavn() + " has a radius of " + planet.getRadius() + "km and a mass of " +
-                        planet.getMasse() + "kg");
-            }
-                else if (menuChoices.equals("remove")) {
+                    planets.add(planet);
+
+                    System.out.println(planet.getNavn() + " has a radius of " + planet.getRadius() + "km and a mass of " +
+                            planet.getMasse() + "kg");
+                }
+                case "remove" -> {
                     System.out.println("Which planet would you like to remove?: ");
                     String planetToRemove = scanner.nextLine();
                     planets.removeIf(planet -> planet.getNavn().equals(planetToRemove));
-            }
-                else if (menuChoices.equals("print")) {
+                }
+                case "print" -> {
                     for (Planet planet : planets) {
                         System.out.println("Name: " + planet.getNavn() + ". | Radius: " + planet.getRadius() + "km. "
                                 + "| Mass: " + planet.getMasse() + "kg");
                     }
-            }
+                }
+                case "update" -> {
+                    System.out.println("Which planet would you like to update?");
+                    String planetToUpdate = scanner.nextLine();
 
-            // WHAT TO DO WITH CODE BELOW.... ?
-            // Code added for oppgave 3.2
-            System.out.print("Do you wish to continue registering planets? Answer yes or no: ");
+                }
+                default -> {
+                    // WHAT TO DO WITH CODE BELOW.... ?
+                    // Code added for oppgave 3.2
+                    System.out.print("Do you wish to continue registering planets? Answer yes or no: ");
+                }
+            }
             String answer = scanner.nextLine();
 
             if (answer.equals("no")) {
