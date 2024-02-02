face6d4a88ce372d9310fdfbde05dcbbed56cdc9
Stefan Strand <stefanboytchev@hotmail.com>
2024-02-02 13:48:28 +0100

    adding comments for learing purposes

--- Oblig1/HelloWorld.java
+++ Oblig1/HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
-    public static void main(String[] args) {
+public class HelloWorld { // declaring a class. Top level classes are always public (access-modifier public).
+    public static void main(String[] args) { //main method - JVM, java virtual machine always looks for this to start.
         System.out.println("Hello World!");
     }
 }
--- Oblig1/Planet.java
+++ Oblig1/Planet.java
@@ -1,19 +1,20 @@
-public class Planet {
-    private String navn;
+public class Planet { // Declaring a class called Planet.
+    private String navn; // instance variables here. Set to private to encapsulate (as per OOP principles).
     private double radius;
     private double masse;
 
-    public Planet(String navn, double radius, double masse) {
-        this.navn = navn;
+    public Planet(String navn, double radius, double masse) { // this is a constructor, to make a new object.
+        this.navn = navn; // "this" to refer to the instance variable when we have constructor/method variables with
+        //the same name.
         this.radius = radius;
         this.masse = masse;
     }
 
-    public String getNavn() {
+    public String getNavn() { //getter for the navn variable.
         return navn;
     }
 
-    public double getRadius() {
+    public double getRadius() { //getter for the radius variable.
         return radius;
     }
 
