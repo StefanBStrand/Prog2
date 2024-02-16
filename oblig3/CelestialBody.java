cf72a4d90243d8e4b9e2733eec10a7b85f810a24
Stefan Strand <stefanboytchev@hotmail.com>
2024-02-16 09:30:57 +0100

    Fixing abstractions

--- oblig3/CelestialBody.java
+++ oblig3/CelestialBody.java
@@ -5,6 +5,15 @@ public class CelestialBody {
 
     private double mass;
 
+    public static final double RJUP = 71492; // This is a constant - set as final.
+    public static final double MJUP = 1.898e27;
+
+    public static final double MEARTH = 5.972e24;
+
+    public static final double REARTH = 6371;
+
+    public static final double G = 6.67408e-11;
+
     public CelestialBody(String name, double radius, double mass) {
         this.name = name;
         this.radius = radius;
--- oblig3/Planet.java
+++ oblig3/Planet.java
@@ -1,71 +1,36 @@
 public class Planet extends CelestialBody {
-    private String name;
-
-    private double radius;
-
-    private double mass;
-
-    public static final double RJUP = 71492; // This is a constant - set as final.
-    public static final double MJUP = 1.898e27;
-
-    private static final double MEARTH = 5.972e24;
-
-    private static final double REARTH = 6371;
-
-    private static final double G = 6.67408e-11;
 
 
     public Planet(String name, double radius, double mass) {
         super(name, radius, mass);
     }
-    // Oppg 2.4 - Method for Planet.
+
     public double getRadiusInKm() {
-        return radius * RJUP;
+        return getRadius() * RJUP;
     }
-    // Oppg 2.4 - Method for Planet
+
     public double getMassInKg() {
-        return mass * MJUP;
+        return getMass() * MJUP;
     }
-    // Bonus 3.2
+
     public double getMassInMEARTH() {
-        double planetInMEARTH = getMassInKg() / MEARTH;
-        return planetInMEARTH;
+        return getMassInKg() / MEARTH;
+
     }
-    // Bonus 3.2
+
     public double getRadiusInREARTH() {
-        double planetInREARTH = getRadiusInKm() / REARTH;
-        return planetInREARTH;
+        return getRadiusInKm() / REARTH;
+
     }
 
     public double surfaceGravity() {
         double radiusInMeters = getRadiusInKm() * 1000;
         double radiusSquared = Math.pow(radiusInMeters, 2);
-        double g = G * getMassInKg() / radiusSquared;
-        return g;
-
+        return G * getMassInKg() / radiusSquared;
     }
 
-    // Oppg. 2.1 - Classes, getters and setters.
-    public String getName() {
-        return name;
-    }
-    public double getRadius() {
-        return radius;
-    }
-    public double getMass() {
-        return mass;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setRadius(double radius) {
-        this.radius = radius;
-    }
-    public void setMass(double mass) {
-        this.mass = mass;
-    }
     @Override // oppg. 2.3
     public String toString() {
-        return "Planet name: " + name + " | Planet radius: " + radius + " | Planet mass: " + mass;
+        return "Planet name: " + getName() + " | Planet radius: " + getName() + " | Planet mass: " + getMass();
     }
 }
